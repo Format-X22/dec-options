8eab0cc485b2d69920da4adfd665d17464165611
oPavlov <oleg.pav.m@gmail.com>
2021-04-22 20:33:59 +0300

    Swagger + validation integration.

--- apps/aggregation-api/src/main.ts
+++ apps/aggregation-api/src/main.ts
@@ -1,11 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AggregationApiModule } from './aggregation-api.module';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { OpenAPIObject } from '@nestjs/swagger/dist/interfaces';
 
 const PORT: number = 3000;
 
 async function bootstrap(): Promise<void> {
     const app: INestApplication = await NestFactory.create(AggregationApiModule);
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+        }),
+    );
+
+    const swaggerConfig: ReturnType<DocumentBuilder['build']> = new DocumentBuilder()
+        .setTitle('3Commas Options')
+        .setDescription('CEX/DEX options aggregator')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+
+    const documentation: OpenAPIObject = SwaggerModule.createDocument(app, swaggerConfig);
+
+    SwaggerModule.setup('api', app, documentation);
+
     await app.listen(PORT);
 }
 bootstrap();
