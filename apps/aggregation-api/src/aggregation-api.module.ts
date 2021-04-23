21e12b055c0a71e2202605a946aea72dbbb664b2
oPavlov <oleg.pav.m@gmail.com>
2021-04-23 14:41:15 +0300

    Database + configuration integration.

--- apps/aggregation-api/src/aggregation-api.module.ts
+++ apps/aggregation-api/src/aggregation-api.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AggregationApiController } from './aggregation-api.controller';
 import { AggregationApiService } from './aggregation-api.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModuleOptions } from '@nestjs/mongoose/dist/interfaces/mongoose-options.interface';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            cache: true,
+        }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService): MongooseModuleOptions => (
+                console.log(configService.get<string>('OA_MONGO_CONNECT')),
+                {
+                    uri: configService.get<string>('OA_MONGO_CONNECT'),
+                }
+            ),
+            inject: [ConfigService],
+        }),
+    ],
     controllers: [AggregationApiController],
     providers: [AggregationApiService],
 })
--- apps/options-aggregator/src/options-aggregator.module.ts
+++ apps/options-aggregator/src/options-aggregator.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { OptionsAggregatorController } from './options-aggregator.controller';
 import { OptionsAggregatorService } from './options-aggregator.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModuleOptions } from '@nestjs/mongoose/dist/interfaces/mongoose-options.interface';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            cache: true,
+        }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService): MongooseModuleOptions => ({
+                uri: configService.get<string>('OA_MONGO_CONNECT'),
+            }),
+            inject: [ConfigService],
+        }),
+    ],
     controllers: [OptionsAggregatorController],
     providers: [OptionsAggregatorService],
 })
