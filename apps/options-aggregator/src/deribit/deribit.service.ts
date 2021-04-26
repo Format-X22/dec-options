4efff1602cf52c33ff8eafacd6d1e538d90f5fa4
oPavlov <oleg.pav.m@gmail.com>
2021-04-26 22:33:41 +0300

    OKEX integration.

--- apps/options-aggregator/src/deribit/deribit.service.ts
+++ apps/options-aggregator/src/deribit/deribit.service.ts
@@ -13,19 +13,21 @@ export class DeribitService implements IAggregator {
         const result: Array<OptionsData> = [];
 
         for (const [id, data] of Object.entries(markets)) {
-            if (data.type === 'option' && data.active) {
-                result.push({
-                    id,
-                    market: 'Deribit',
-                    marketType: EMarketType.CEX,
-                    type: data.info.option_type.toUpperCase(),
-                    size: Number(data.info.contract_size),
-                    strike: Number(data.info.strike),
-                    expirationDate: new Date(Number(data.info.expiration_timestamp)),
-                    base: data.base,
-                    quote: data.quote,
-                });
+            if (!data.active || data.type !== 'option') {
+                continue;
             }
+
+            result.push({
+                id,
+                market: 'DERIBIT',
+                marketType: EMarketType.CEX,
+                type: data.info.option_type.toUpperCase(),
+                size: Number(data.info.contract_size),
+                strike: Number(data.info.strike),
+                expirationDate: new Date(Number(data.info.expiration_timestamp)),
+                base: data.base,
+                quote: data.quote,
+            });
         }
 
         return result;
--- apps/options-aggregator/src/okex/okex.service.ts
+++ apps/options-aggregator/src/okex/okex.service.ts
@@ -1,11 +1,49 @@
 import { Injectable } from '@nestjs/common';
-import { OptionsData } from '@app/shared/options-data.schema';
+import { EMarketType, EOptionType, OptionsData } from '@app/shared/options-data.schema';
 import { IAggregator } from '../options-aggregator.service';
+import * as ccxt from 'ccxt';
+import { Dictionary, Exchange, Market } from 'ccxt';
+
+type TOptionMarket = Market & {
+    option: boolean;
+};
 
 @Injectable()
 export class OkexService implements IAggregator {
+    private readonly exchange: Exchange = new ccxt.okex();
+
     async getCurrentData(): Promise<Array<OptionsData>> {
-        // TODO -
-        return;
+        const markets: Dictionary<TOptionMarket> = (await this.exchange.loadMarkets()) as Dictionary<TOptionMarket>;
+        const result: Array<OptionsData> = [];
+
+        for (const [id, data] of Object.entries(markets)) {
+            if (!data.active || !data.option) {
+                continue;
+            }
+
+            result.push({
+                id,
+                market: 'OKEX',
+                marketType: EMarketType.CEX,
+                type: this.determinateType(data),
+                size: Number(data.info.lot_size),
+                strike: Number(data.info.strike),
+                expirationDate: new Date(data.info.delivery),
+                base: data.base,
+                quote: data.quote,
+            });
+        }
+
+        return result;
+    }
+
+    private determinateType(data: TOptionMarket): EOptionType {
+        if (data.info.option_type === 'P') {
+            return EOptionType.PUT;
+        } else if (data.info.option_type === 'C') {
+            return EOptionType.CALL;
+        } else {
+            throw new Error(`Unknown OKEX option type - ${data.info.option_type} - ${JSON.stringify(data)}`);
+        }
     }
 }
