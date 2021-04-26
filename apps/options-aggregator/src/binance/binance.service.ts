44af6d1ce2d7f27dc01040a8eee4a5ed753e1f5f
oPavlov <oleg.pav.m@gmail.com>
2021-04-26 22:58:42 +0300

    Binance integration.

--- apps/options-aggregator/src/binance/binance.service.ts
+++ apps/options-aggregator/src/binance/binance.service.ts
@@ -1,11 +1,49 @@
 import { Injectable } from '@nestjs/common';
-import { OptionsData } from '@app/shared/options-data.schema';
+import { EMarketType, EOptionType, OptionsData } from '@app/shared/options-data.schema';
 import { IAggregator } from '../options-aggregator.service';
+import { Exchange } from 'ccxt';
+import * as ccxt from 'ccxt';
+
+const GetOptionsApiUrl: string = 'https://vapi.binance.com/vapi/v1/optionInfo';
+
+type TOptionsResponse = {
+    msg: string;
+    data: Array<{
+        id: string;
+        side: string;
+        unit: string;
+        strikePrice: string;
+        expiryDate: string;
+        underlying: string;
+        quoteAsset: string;
+    }>;
+};
 
 @Injectable()
 export class BinanceService implements IAggregator {
+    private readonly exchange: Exchange = new ccxt.binance();
+
     async getCurrentData(): Promise<Array<OptionsData>> {
-        // TODO -
-        return;
+        const rawOptionsResponse: TOptionsResponse = await this.exchange.fetch(GetOptionsApiUrl, 'GET');
+
+        if (rawOptionsResponse.msg !== 'success') {
+            throw new Error(`Invalid BINANCE response - ${JSON.stringify(rawOptionsResponse, null, 2)}`);
+        }
+
+        return rawOptionsResponse.data.map(
+            (data: TOptionsResponse['data'][0]): OptionsData => {
+                return {
+                    id: data.id,
+                    market: 'BINANCE',
+                    marketType: EMarketType.CEX,
+                    type: data.side.toUpperCase() as EOptionType,
+                    size: Number(data.unit),
+                    strike: Number(data.strikePrice),
+                    expirationDate: new Date(Number(data.expiryDate)),
+                    base: data.underlying.replace(data.quoteAsset, ''),
+                    quote: data.quoteAsset,
+                };
+            },
+        );
     }
 }
