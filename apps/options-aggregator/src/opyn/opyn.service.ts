855870b1f293fa9ac657c1d9340a6b0b6b873158
oPavlov <oleg.pav.m@gmail.com>
2021-04-29 06:39:07 +0300

    Opyn integration.

--- apps/options-aggregator/src/opyn/opyn.service.ts
+++ apps/options-aggregator/src/opyn/opyn.service.ts
@@ -1,11 +1,73 @@
 import { Injectable } from '@nestjs/common';
-import { OptionsData } from '@app/shared/options-data.schema';
+import { EMarket, EMarketType, EOptionType, OptionsData } from '@app/shared/options-data.schema';
 import { IAggregator } from '../options-aggregator.service';
+import { gql, request } from 'graphql-request';
+
+type TOptionsResponse = {
+    otokens: Array<{
+        id: string;
+        name: string;
+        isPut: boolean;
+        expiryTimestamp: string;
+        strikePrice: string;
+        decimals: number;
+        underlyingAsset: {
+            symbol: string;
+        };
+        strikeAsset: {
+            symbol: string;
+        };
+        collateralAsset: {
+            symbol: string;
+        };
+    }>;
+};
+
+const API: string = 'https://api.thegraph.com/subgraphs/name/opynfinance/gamma-mainnet';
+const QUERY: string = gql`
+    {
+        otokens {
+            id
+            underlyingAsset {
+                symbol
+            }
+            strikeAsset {
+                symbol
+            }
+            collateralAsset {
+                symbol
+            }
+            name
+            isPut
+            strikePrice
+            decimals
+            expiryTimestamp
+        }
+    }
+`;
+const MS_MULTIPLY: number = 1000;
 
 @Injectable()
 export class OpynService implements IAggregator {
     async getCurrentData(): Promise<Array<OptionsData>> {
-        // TODO -
-        return;
+        const rawOptionsResponse: TOptionsResponse = await request(API, QUERY);
+
+        return rawOptionsResponse.otokens.map(
+            (data: TOptionsResponse['otokens'][0]): OptionsData => {
+                return {
+                    id: data.id,
+                    name: data.name,
+                    market: EMarket.OPYN,
+                    marketType: EMarketType.DEX,
+                    type: data.isPut ? EOptionType.PUT : EOptionType.CALL,
+                    size: 1,
+                    strike: Number(data.strikePrice) / Math.pow(10, data.decimals),
+                    expirationDate: new Date(Number(data.expiryTimestamp) * MS_MULTIPLY),
+                    base: data.underlyingAsset.symbol,
+                    quote: data.collateralAsset.symbol,
+                    strikeAsset: data.strikeAsset.symbol,
+                };
+            },
+        );
     }
 }
