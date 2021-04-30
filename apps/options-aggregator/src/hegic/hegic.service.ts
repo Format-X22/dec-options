2f22003cc872547e4457e9ba2bb3583a7c0d4822
oPavlov <oleg.pav.m@gmail.com>
2021-04-30 16:35:49 +0300

    Heig integration.

--- apps/options-aggregator/src/hegic/hegic.service.ts
+++ apps/options-aggregator/src/hegic/hegic.service.ts
@@ -1,11 +1,61 @@
 import { Injectable } from '@nestjs/common';
-import { OptionsData } from '@app/shared/options-data.schema';
+import { EMarket, EMarketType, EOptionType, OptionsData } from '@app/shared/options-data.schema';
 import { IAggregator } from '../options-aggregator.service';
+import { gql, request } from 'graphql-request';
+
+type TOptionsResponse = {
+    hegicOptions: Array<{
+        id: string;
+        underlying: {
+            symbol: string;
+        };
+        strike: string;
+        expiration: string;
+        type: string;
+    }>;
+};
+
+const API: string = 'https://api.thegraph.com/subgraphs/name/cvauclair/hegic';
+const MS_MULTIPLY: number = 1000;
 
 @Injectable()
 export class HegicService implements IAggregator {
     async getCurrentData(): Promise<Array<OptionsData>> {
-        // TODO -
-        return;
+        const rawOptionsResponse: TOptionsResponse = await request(API, this.getQuery());
+
+        return rawOptionsResponse.hegicOptions.map(
+            (data: TOptionsResponse['hegicOptions'][0]): OptionsData => {
+                return {
+                    id: data.id,
+                    name: data.id,
+                    market: EMarket.HEGIC,
+                    marketType: EMarketType.DEX,
+                    type: data.type.toUpperCase() as EOptionType,
+                    size: 1,
+                    strike: Number(data.strike),
+                    expirationDate: new Date(Number(data.expiration) * MS_MULTIPLY),
+                    base: data.underlying.symbol,
+                    quote: 'USD',
+                    strikeAsset: data.underlying.symbol,
+                };
+            },
+        );
+    }
+
+    private getQuery(): string {
+        const now: number = Math.floor(Date.now() / MS_MULTIPLY);
+
+        return gql`{  
+            hegicOptions(where: {state: Active expiration_lt: ${now}}) {
+                id
+                underlying {
+                    symbol
+                }
+                strike
+                amount
+                expiration
+                type
+            }
+        }`;
     }
 }
