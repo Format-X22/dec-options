78702bbf0919266ca84200dec283f5bdd501f5f8
oleg <o.pavlov@3commas.io>
2021-05-28 18:37:33 +0300

    RND-139 Remove unused aggregators.

--- apps/options-aggregator/src/options-aggregator.module.ts
+++ apps/options-aggregator/src/options-aggregator.module.ts
@@ -5,19 +5,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { MongooseModuleOptions } from '@nestjs/mongoose/dist/interfaces/mongoose-options.interface';
 import { HegicService } from './hegic/hegic.service';
 import { OpynService } from './opyn/opyn.service';
-import { PlotxService } from './_plotx/plotx.service';
-import { CharmService } from './_charm/charm.service';
-import { PrimitiveService } from './_primitive/primitive.service';
-import { ProsperService } from './_prosper/prosper.service';
 import { AuctusService } from './auctus/auctus.service';
-import { FinnexusService } from './_finnexus/finnexus.service';
-import { LienService } from './_lien/lien.service';
-import { OpiumService } from './_opium/opium.service';
-import { PodsService } from './_pods/pods.service';
-import { XoptsService } from './_xopts/xopts.service';
 import { SirenService } from './siren/siren.service';
 import { DeribitService } from './deribit/deribit.service';
-import { FtxService } from './_ftx/ftx.service';
 import { BinanceService } from './binance/binance.service';
 import { OkexService } from './okex/okex.service';
 import { Option, OptionSchema } from '@app/shared/option.schema';
@@ -41,19 +31,9 @@ import { Option, OptionSchema } from '@app/shared/option.schema';
         OptionsAggregatorService,
         HegicService,
         OpynService,
-        PlotxService,
-        CharmService,
-        PrimitiveService,
-        ProsperService,
         AuctusService,
-        FinnexusService,
-        LienService,
-        OpiumService,
-        PodsService,
-        XoptsService,
         SirenService,
         DeribitService,
-        FtxService,
         BinanceService,
         OkexService,
     ],
--- apps/options-aggregator/src/options-aggregator.service.ts
+++ apps/options-aggregator/src/options-aggregator.service.ts
@@ -2,21 +2,11 @@ import { Injectable, Logger } from '@nestjs/common';
 import { Option, OptionDocument } from '@app/shared/option.schema';
 import { AuctusService } from './auctus/auctus.service';
 import { BinanceService } from './binance/binance.service';
-import { CharmService } from './_charm/charm.service';
 import { DeribitService } from './deribit/deribit.service';
-import { FinnexusService } from './_finnexus/finnexus.service';
-import { FtxService } from './_ftx/ftx.service';
 import { HegicService } from './hegic/hegic.service';
-import { LienService } from './_lien/lien.service';
 import { OkexService } from './okex/okex.service';
-import { OpiumService } from './_opium/opium.service';
 import { OpynService } from './opyn/opyn.service';
-import { PlotxService } from './_plotx/plotx.service';
-import { PodsService } from './_pods/pods.service';
-import { PrimitiveService } from './_primitive/primitive.service';
-import { ProsperService } from './_prosper/prosper.service';
 import { SirenService } from './siren/siren.service';
-import { XoptsService } from './_xopts/xopts.service';
 import { ConfigService } from '@nestjs/config';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -39,21 +29,12 @@ export class OptionsAggregatorService {
 
         private readonly auctusService: AuctusService,
         private readonly binanceService: BinanceService,
-        private readonly charmService: CharmService,
         private readonly deribitService: DeribitService,
-        private readonly finnexusService: FinnexusService,
-        private readonly ftxService: FtxService,
         private readonly hegicService: HegicService,
         private readonly lienService: LienService,
         private readonly okexService: OkexService,
-        private readonly opiumService: OpiumService,
         private readonly opynService: OpynService,
-        private readonly plotxService: PlotxService,
-        private readonly podsService: PodsService,
-        private readonly primitiveService: PrimitiveService,
-        private readonly prosperService: ProsperService,
         private readonly sirenService: SirenService,
-        private readonly xoptsService: XoptsService,
     ) {}
 
     async start(): Promise<void> {
