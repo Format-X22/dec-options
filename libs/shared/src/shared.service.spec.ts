e3d304c1dc2b106b2c5484f484b5b88760a284e4
oleg <o.pavlov@3commas.io>
2021-06-09 01:02:32 +0300

    Auto code pretty result.

--- libs/shared/src/shared.service.spec.ts
+++ libs/shared/src/shared.service.spec.ts
@@ -4,13 +4,15 @@ import { SharedService } from './shared.service';
 describe('SharedService', (): void => {
     let service: SharedService;
 
-    beforeEach(async (): Promise<void> => {
-        const module: TestingModule = await Test.createTestingModule({
-            providers: [SharedService],
-        }).compile();
+    beforeEach(
+        async (): Promise<void> => {
+            const module: TestingModule = await Test.createTestingModule({
+                providers: [SharedService],
+            }).compile();
 
-        service = module.get<SharedService>(SharedService);
-    });
+            service = module.get<SharedService>(SharedService);
+        },
+    );
 
     it('should be defined', (): void => {
         expect(service).toBeDefined();
