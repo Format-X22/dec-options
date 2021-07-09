e96e65f9ac73820a4067a684526a601ef876c089
oleg <o.pavlov@3commas.io>
2021-07-09 13:43:31 +0300

    RND-208 RND-209 Aggregated order book.

--- apps/frontend/components/OptionsTable/OrderBook.tsx
+++ apps/frontend/components/OptionsTable/OrderBook.tsx
@@ -2,12 +2,42 @@ import React, { useContext } from 'react';
 import { TableRow } from '../StrikesTable/TableRow';
 import { TableCell } from '../StrikesTable/TableCell';
 import { TitleText } from '../StrikesTable/TitleText';
-import { gql, useQuery } from '@apollo/client';
+import { DocumentNode, gql, useQuery } from '@apollo/client';
 import { OrderBook as OrderBookModel, OrderBookOrder } from '../../../../libs/shared/src/orderbook.schema';
 import { QueryResult } from '@apollo/client/react/types/types';
 import { ContextState } from '../../pages/stateType';
 import { ContextApp } from '../../pages/_app';
 import styled from 'styled-components';
+import { Option, OptionGQL } from '../../../../libs/shared/src/option.schema';
+
+const GET_OPTIONS = gql`
+    query getOptions(
+        $type: OptionType
+        $base: String!
+        $fromExpirationDate: DateTime
+        $toExpirationDate: DateTime
+        $strike: Float!
+    ) {
+        options(
+            filterByType: $type
+            filterByBase: $base
+            filterByExpirationDateFrom: $fromExpirationDate
+            filterByExpirationDateTo: $toExpirationDate
+            filterByStrike: $strike
+        ) {
+            data {
+                id
+                strike
+                askQuote
+                bidQuote
+                market {
+                    key
+                    name
+                }
+            }
+        }
+    }
+`;
 
 const GET_ORDER_BOOK = gql`
     query getOrderBook($optionId: String!, $optionMarketKey: MarketKey!) {
@@ -51,11 +81,56 @@ const Divider: React.FunctionComponent = styled.div`
 
 export function OrderBook(): JSX.Element {
     const { state }: Partial<ContextState> = useContext(ContextApp);
-    const { data, error }: QueryResult<{ orderBook: OrderBookModel }> = useQuery(GET_ORDER_BOOK, {
-        variables: {
-            optionId: state.selectedOptionForOrderBook?.optionId || '',
-            optionMarketKey: state.selectedOptionForOrderBook?.optionMarketKey || 'DERIBIT',
-        },
+    const fromExpirationDate = new Date(state.selectedOptionGroup?.date || 0);
+
+    fromExpirationDate.setHours(0);
+    fromExpirationDate.setMinutes(0);
+    fromExpirationDate.setSeconds(0);
+
+    const toExpirationDate = new Date(state.selectedOptionGroup?.date || 0);
+
+    toExpirationDate.setHours(23);
+    toExpirationDate.setMinutes(59);
+    toExpirationDate.setSeconds(59);
+
+    const { data: optionGroupData, error: optionGroupError }: QueryResult<{ options: { data: Array<Option> } }> =
+        useQuery(GET_OPTIONS, {
+            variables: {
+                type: state.selectedOptionGroup?.type.toUpperCase() || 'CALL',
+                base: state.selectedOptionGroup?.base || '',
+                strike: state.selectedOptionGroup?.strike || 0,
+                fromExpirationDate,
+                toExpirationDate,
+            },
+        });
+
+    const asks: Array<OrderBookOrder> = [];
+    const bids: Array<OrderBookOrder> = [];
+
+    const { data, error }: QueryResult<{ orderBook: OrderBookModel }> = useQuery(
+        makeOrderBookQuery(optionGroupData?.options.data),
+    );
+
+    if (data) {
+        for (const orderBook of Object.values(data)) {
+            asks.push(...orderBook?.asks || []);
+            bids.push(...orderBook?.bids || []);
+        }
+    }
+
+    asks.sort((a: OrderBookOrder, b: OrderBookOrder): number => {
+        if (a.price > b.price) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+    bids.sort((a: OrderBookOrder, b: OrderBookOrder): number => {
+        if (a.price < b.price) {
+            return 1;
+        } else {
+            return -1;
+        }
     });
 
     return (
@@ -69,25 +144,28 @@ export function OrderBook(): JSX.Element {
                     <TitleText>Amount</TitleText>
                 </TableCell>
             </TableRow>
-            {error && <TableRow>{error.toString()}</TableRow>}
-            {!error && !data?.orderBook && <TableRow>{'Select option...'}</TableRow>}
-            {!error && data?.orderBook && (
+            {optionGroupError && <TableRow>{optionGroupError.toString()}</TableRow>}
+            {!optionGroupError && error && <TableRow>{error.toString()}</TableRow>}
+            {!optionGroupError && !error && (
                 <>
-                    {data.orderBook.asks?.slice().reverse().map(
-                        (order: OrderBookOrder): JSX.Element => (
-                            <TableRow>
-                                <TableCell className={'TODO-order-book-column-size'}>
-                                    <AsksText>{order.price}</AsksText>
-                                </TableCell>
-                                <TableCell className={'TODO-order-book-column-size'}>
-                                    <TitleText>{order.amount}</TitleText>
-                                </TableCell>
-                            </TableRow>
-                        ),
-                    )}
-                    {data.orderBook.asks?.length === 0 && <TableRow>{'No asks...'}</TableRow>}
+                    {asks
+                        .slice()
+                        .reverse()
+                        .map(
+                            (order: OrderBookOrder): JSX.Element => (
+                                <TableRow>
+                                    <TableCell className={'TODO-order-book-column-size'}>
+                                        <AsksText>{order.price}</AsksText>
+                                    </TableCell>
+                                    <TableCell className={'TODO-order-book-column-size'}>
+                                        <TitleText>{order.amount}</TitleText>
+                                    </TableCell>
+                                </TableRow>
+                            ),
+                        )}
+                    {asks.length === 0 && <TableRow>{'No asks...'}</TableRow>}
                     <Divider />
-                    {data.orderBook?.bids.map(
+                    {bids.map(
                         (order: OrderBookOrder): JSX.Element => (
                             <TableRow>
                                 <TableCell className={'TODO-order-book-column-size'}>
@@ -99,9 +177,37 @@ export function OrderBook(): JSX.Element {
                             </TableRow>
                         ),
                     )}
-                    {data.orderBook?.bids.length === 0 && <TableRow>{'No bids...'}</TableRow>}
+                    {bids.length === 0 && <TableRow>{'No bids...'}</TableRow>}
                 </>
             )}
         </>
     );
+}
+
+function makeOrderBookQuery(options: Array<Option> = []): DocumentNode {
+    let body: string = '';
+
+    options.forEach((option: OptionGQL, index: number): void => {
+        body += `
+            id${index}: orderBook(optionId: "${option.id}", optionMarketKey: ${option.market.key}) {
+                optionMarketKey
+                asks {
+                    price
+                    amount
+                }
+                bids {
+                    price
+                    amount
+                }
+            }
+        `;
+    });
+
+    body = body || 'orderBook(optionId: "", optionMarketKey: DERIBIT) { optionMarketKey }';
+
+    return gql`
+        query getOrderBook {
+            ${body}
+        }
+    `;
 }
