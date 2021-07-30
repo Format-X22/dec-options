3a76177e46e7241cbfc34aadafaa8d5ef9c9f4c7
oleg <o.pavlov@3commas.io>
2021-07-31 01:07:17 +0300

    RND-280 Stats api.

--- apps/aggregation-api/src/stats/stats.resolver.ts
+++ apps/aggregation-api/src/stats/stats.resolver.ts
@@ -1,4 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { Stats } from '@app/shared/stats.schema';
+import { StatsService } from './stats.service';
 
-@Resolver()
-export class StatsResolver {}
+@Resolver((): typeof Stats => Stats)
+export class StatsResolver {
+    constructor(private statsService: StatsService) {}
+
+    @Query((): Array<typeof Stats> => [Stats])
+    async stats(): Promise<Array<Stats>> {
+        return this.statsService.getStats();
+    }
+}
--- apps/aggregation-api/src/stats/stats.service.ts
+++ apps/aggregation-api/src/stats/stats.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Stats, StatsDocument } from '@app/shared/stats.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class StatsService {}
+export class StatsService {
+    constructor(@InjectModel(Stats.name) private statsModel: Model<StatsDocument>) {}
+
+    async getStats(): Promise<Array<Stats>> {
+        return this.statsModel.find({});
+    }
+}
