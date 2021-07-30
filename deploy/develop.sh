2074370bd334f9835a0995a4c4d1410c568e9641
Dmitriy Tarasov <d.tarasov@3commas.io>
2021-07-30 16:28:03 +0300

    update ci-cd

---
+++ deploy/develop.sh
@@ -0,0 +1,32 @@
+#/usr/bin/env bash
+
+# cleanup on error
+function cleanup() {
+  echo "cleanup"
+}
+
+source ~/.bashrc
+source ~/.nvm/nvm.sh
+
+node -v
+
+nvm use 14
+
+echo "Stop old process"
+pm2 --silent stop tools_backend_staging
+echo "Stopped"
+set -e
+echo "Move old directory"
+rm -rf /var/www/opex/stage/old
+mv /var/www/opex/stage/current/ /var/www/opex/stage/old
+
+echo "clear old directory"
+rm -rf /var/www/opex/stage/current/*
+
+echo "Copy source to directory"
+mkdir /var/www/opex/stage/current
+cp -r /var/www/opex/stage/source/* /var/www/opex/stage/current
+cp /var/www/opex/stage/source/.env /var/www/opex/stage/current
+
+echo "Start new process"
+cd /var/www/opex/stage/current && yarn yarn pm2:start:all:staging
