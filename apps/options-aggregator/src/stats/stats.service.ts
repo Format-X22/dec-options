e47394bcd786c09b5a085661620dddb545a8a38b
oleg <o.pavlov@3commas.io>
2021-08-04 23:12:39 +0300

    RND-280 Draft with Binance + Okex sync.

--- apps/options-aggregator/src/stats/stats.service.ts
+++ apps/options-aggregator/src/stats/stats.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import * as ccxt from 'ccxt';
+import * as moment from 'moment';
 import { Exchange } from 'ccxt';
 import { InjectModel } from '@nestjs/mongoose';
 import { Option, OptionDocument } from '@app/shared/option.schema';
@@ -16,6 +17,50 @@ type TOptionNamesWithMeta = Array<{
     strike: number;
 }>;
 
+type TBinanceCandleResponse = {
+    code: string;
+    msg: string;
+    data:
+        | []
+        | [
+              {
+                  volume: string;
+              },
+          ];
+};
+
+type TOkexCandleResponse =
+    | []
+    | [
+          | []
+          | [
+                string, // Timestamp
+                string, // Open
+                string, // High
+                string, // Low
+                string, // Close
+                string, // Volume
+            ],
+      ];
+
+type TOkexInstrumentResponse = {
+    open_interest: string;
+};
+
+type TCacheItem = {
+    base: string;
+    marketKey: EMarketKey;
+    name: string;
+    expirationDate: Date;
+    strike: number;
+    volume: number;
+    openInterest: number;
+};
+
+const BINANCE_API = 'https://vapi.binance.com/vapi/v1';
+const OKEX_API = 'https://www.okex.com/api/option/v3';
+const DERIBIT_API = 'https://www.deribit.com/api/v2';
+
 @Injectable()
 export class StatsService {
     private readonly logger: Logger = new Logger(StatsService.name);
@@ -23,12 +68,14 @@ export class StatsService {
     private readonly okexExchange: Exchange = new ccxt.okex();
     private readonly deribitExchange: Exchange = new ccxt.deribit();
     private inProcess: boolean = false;
+    private cache: Array<TCacheItem> = [];
 
     constructor(
         @InjectModel(Option.name) private optionModel: Model<OptionDocument>,
         @InjectModel(Stats.name) private statsModel: Model<StatsDocument>,
     ) {}
 
+    // TODO -
     @Cron(CronExpression.EVERY_MINUTE)
     private async iteration(): Promise<void> {
         if (this.inProcess) {
@@ -36,15 +83,18 @@ export class StatsService {
         }
 
         this.inProcess = true;
+        this.cache = [];
 
         try {
+            this.logger.log('Trade history sync started');
+
             const results = await Promise.allSettled([
                 this.syncCexStock(EMarketKey.BINANCE, this.syncBinanceOption, this.binanceExchange.rateLimit),
                 this.syncCexStock(EMarketKey.OKEX, this.syncOkexOption, this.okexExchange.rateLimit),
                 this.syncCexStock(EMarketKey.DERIBIT, this.syncDeribitOption, this.deribitExchange.rateLimit),
             ]);
 
-            this.logger.log('Trade history sync started');
+            this.logger.log('Trade history sync complete');
 
             for (const result of results) {
                 if (result.status === 'rejected') {
@@ -52,7 +102,6 @@ export class StatsService {
                 }
             }
 
-            this.logger.log('Trade history sync complete');
             this.logger.log('Stats build started');
 
             await this.buildStats();
@@ -65,12 +114,20 @@ export class StatsService {
         this.inProcess = false;
     }
 
-    private async syncCexStock(marketKey: EMarketKey, handler: Function, rateLimit: number): Promise<void> {
+    private async syncCexStock(
+        marketKey: EMarketKey,
+        handler: (data: TOptionNamesWithMeta[0], marketKey: EMarketKey) => Promise<TCacheItem>,
+        rateLimit: number,
+    ): Promise<void> {
         const namesWithMeta: TOptionNamesWithMeta = await this.extractOptionNamesWithMeta(marketKey);
 
         for (const data of namesWithMeta) {
             try {
-                await handler.call(this, data);
+                const result: TCacheItem = await handler.call(this, data, marketKey);
+
+                if (result) {
+                    this.cache.push(result);
+                }
             } catch (error) {
                 // Do nothing
             }
@@ -79,16 +136,84 @@ export class StatsService {
         }
     }
 
-    private async syncBinanceOption({ name, base, expirationDate, strike }: TOptionNamesWithMeta[0]): Promise<void> {
-        // TODO -
+    private async syncBinanceOption(
+        { name, base, expirationDate, strike }: TOptionNamesWithMeta[0],
+        marketKey,
+    ): Promise<TCacheItem> {
+        const candleStartTime: number = moment().startOf('hour').subtract(1, 'hour').valueOf();
+        const candleEndTime: number = moment().startOf('hour').subtract(1, 'ms').valueOf();
+
+        const candleResponse: TBinanceCandleResponse = await this.binanceExchange.fetch(
+            `${BINANCE_API}/klines?interval=1h&symbol=${name}&startTime=${candleStartTime}&endTime=${candleEndTime}`,
+            'GET',
+        );
+
+        if (Number(candleResponse.code) !== 0) {
+            throw new Error(candleResponse.msg || candleResponse.code);
+        }
+
+        if (candleResponse.data.length === 0) {
+            return;
+        }
+
+        const volume = Number(candleResponse.data[0].volume);
+
+        return {
+            name,
+            base,
+            expirationDate,
+            strike,
+            volume,
+            marketKey,
+            openInterest: 0,
+        };
     }
 
-    private async syncOkexOption({ name, base, expirationDate, strike }: TOptionNamesWithMeta[0]): Promise<void> {
-        // TODO -
+    private async syncOkexOption(
+        { name, base, expirationDate, strike }: TOptionNamesWithMeta[0],
+        marketKey,
+    ): Promise<TCacheItem> {
+        const candleStartTime: string = moment.utc().startOf('hour').subtract(1, 'hour').toISOString();
+        const candleEndTime: string = moment.utc().startOf('hour').subtract(1, 'ms').toISOString();
+        const candleResponse: TOkexCandleResponse = await this.okexExchange.fetch(
+            `${OKEX_API}/instruments/${name}/candles?granularity=3600&start=${candleStartTime}&end=${candleEndTime}`,
+            'GET',
+        );
+
+        if (candleResponse.length === 0 || candleResponse[0].length === 0) {
+            return;
+        }
+
+        const volume = Number(candleResponse[0][5]);
+
+        const instrumentResponse: TOkexInstrumentResponse = await this.okexExchange.fetch(
+            `${OKEX_API}/instruments/${base}-USD/summary/${name}`,
+            'GET',
+        );
+
+        if (!instrumentResponse || !instrumentResponse.open_interest) {
+            return;
+        }
+
+        const openInterest = Number(instrumentResponse.open_interest);
+
+        return {
+            name,
+            base,
+            expirationDate,
+            strike,
+            volume,
+            marketKey,
+            openInterest,
+        };
     }
 
-    private async syncDeribitOption({ name, base, expirationDate, strike }: TOptionNamesWithMeta[0]): Promise<void> {
+    private async syncDeribitOption(
+        { name, base, expirationDate, strike }: TOptionNamesWithMeta[0],
+        marketKey,
+    ): Promise<TCacheItem> {
         // TODO -
+        return;
     }
 
     private async extractOptionNamesWithMeta(marketKey: EMarketKey): Promise<TOptionNamesWithMeta> {
