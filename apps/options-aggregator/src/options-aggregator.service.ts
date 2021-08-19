c31dfd2588e611361b5f8f8c8ac1279587b2facc
oleg <o.pavlov@3commas.io>
2021-08-19 13:35:14 +0300

    RND-414 Auctus.

--- apps/options-aggregator/src/options-aggregator.service.ts
+++ apps/options-aggregator/src/options-aggregator.service.ts
@@ -4,6 +4,7 @@ import { DeribitService } from './options/deribit/deribit.service';
 import { HegicService } from './options/hegic/hegic.service';
 import { OkexService } from './options/okex/okex.service';
 import { AggregatorAbstract } from './options/aggregator.abstract';
+import { AuctusService } from './options/auctus/auctus.service';
 
 @Injectable()
 export class OptionsAggregatorService {
@@ -14,10 +15,13 @@ export class OptionsAggregatorService {
         private readonly deribitService: DeribitService,
         private readonly hegicService: HegicService,
         private readonly okexService: OkexService,
+        private readonly auctusService: AuctusService,
     ) {}
 
     async start(): Promise<void> {
-        [this.binanceService, this.deribitService, this.okexService].forEach(this.startOptionsSync.bind(this));
+        [this.binanceService, this.deribitService, this.okexService, this.auctusService].forEach(
+            this.startOptionsSync.bind(this),
+        );
 
         this.logger.verbose('Sync loops started');
     }
--- apps/options-aggregator/src/options/auctus/auctus.service.ts
+++ apps/options-aggregator/src/options/auctus/auctus.service.ts
@@ -1,29 +1,111 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Option } from '@app/shared/option.schema';
+import { EOptionDeliveryType, EOptionStyleType, EOptionType, ESymbol, Option } from '@app/shared/option.schema';
 import { AggregatorAbstract } from '../aggregator.abstract';
 import { OrderBook } from '@app/shared/orderbook.schema';
+import { gql, request } from 'graphql-request';
+import { EMarketKey, EMarketType } from '@app/shared/market.schema';
 
-// TODO Implement
+type TOptionsResponse = {
+    acotokens: Array<{
+        id: string;
+        name: string;
+        isCall: boolean;
+        expiryTime: string;
+        strikePrice: string;
+        collateral: {
+            symbol: string;
+            decimals: string;
+        };
+        underlying: {
+            symbol: string;
+            decimals: string;
+        };
+        strikeAsset: {
+            symbol: string;
+            decimals: string;
+        };
+    }>;
+};
 
-type TRawOption = Object;
+type TRawOption = TOptionsResponse['acotokens'][0];
+
+const API: string = 'https://api.thegraph.com/subgraphs/name/auctusproject/auctus-options';
+const MS_MULTIPLY: number = 1000;
 
 @Injectable()
 export class AuctusService extends AggregatorAbstract<TRawOption> {
     protected readonly logger: Logger = new Logger(AuctusService.name);
+    protected readonly isGetWithPagination: boolean = true;
+    protected readonly pageSize: number = 1000;
 
     protected get rateLimit(): number {
-        return null;
+        return 3000;
     }
 
-    protected async getRawOptions(): Promise<Array<TRawOption>> {
-        return null;
+    protected async getRawOptions(skip: number): Promise<Array<TRawOption>> {
+        const rawOptionsResponse: TOptionsResponse = await request(API, this.getQuery(skip));
+
+        return rawOptionsResponse.acotokens;
     }
 
     protected async getOrderBook(rawOption: TRawOption): Promise<OrderBook> {
+        // TODO Implement
         return null;
     }
 
-    protected constructOptionData(rawOption: TRawOption, orderBook: OrderBook): Option {
-        return null;
+    protected constructOptionData(rawOption: TRawOption): Option {
+        const id: string = rawOption.id;
+        const base: ESymbol = rawOption.underlying.symbol as ESymbol;
+        const quote: string = rawOption.collateral.symbol;
+        const strikeAsset: string = rawOption.strikeAsset.symbol;
+
+        return {
+            id,
+            name: rawOption.name,
+            marketKey: EMarketKey.AUCTUS,
+            marketType: EMarketType.DEX,
+            type: rawOption.isCall ? EOptionType.CALL : EOptionType.PUT,
+            size: 1,
+            strike: Number(rawOption.strikePrice),
+            expirationDate: new Date(Number(rawOption.expiryTime) * MS_MULTIPLY),
+            base,
+            quote,
+            strikeAsset,
+            marketUrl: `https://app.auctus.org/advanced/trade/${base}_${strikeAsset}/${id}`,
+            askBase: null,
+            askQuote: null,
+            bidBase: null,
+            bidQuote: null,
+            deliveryType: EOptionDeliveryType.DELIVERY,
+            styleType: EOptionStyleType.AMERICAN,
+        };
+    }
+
+    private getQuery(skip: number): string {
+        const now: number = Math.floor(Date.now() / MS_MULTIPLY);
+
+        return gql`
+            {
+                acotokens(where: { expiryTime_gt: ${now} } first: ${this.pageSize} skip: ${skip}) {
+                    id
+                    underlying {
+                        symbol
+                        decimals
+                    }
+                    strikeAsset {
+                        symbol
+                        decimals
+                    }
+                    collateral {
+                        symbol
+                        decimals
+                    }
+                    name
+                    isCall
+                    strikePrice
+                    expiryTime
+                }
+            }
+        `;
     }
 }
