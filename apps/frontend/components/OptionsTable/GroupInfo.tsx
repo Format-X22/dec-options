7b62eb622a557fff8bd65acbf36e63ef628206fe
shamyyl <shamil.salakhiev@gmail.com>
2021-09-30 15:16:52 +0300

    fixed build

--- apps/frontend/components/OptionsTable/GroupInfo.tsx
+++ apps/frontend/components/OptionsTable/GroupInfo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
 import { $backgroundLight } from '../../theme';
-import { Button, ButtonLink } from '../Button';
+import { Button } from '../Button';
 import { useRouter } from 'next/router';
 import { ITradeQuery } from '../../dtos/ITradeQuery';
 import { CallsSvgIcon } from '../Icons/CallsSvgIcon';
